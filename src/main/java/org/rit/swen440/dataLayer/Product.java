a89a45bc73559534d3ada782c6e2217ba6dd1aa2
Bryan Quinn <blq9064@rit.edu>
2018-11-30 11:15:02 -0500

    Move logic outside of data layer POJO.

--- src/main/java/org/rit/swen440/dataLayer/Product.java
+++ src/main/java/org/rit/swen440/dataLayer/Product.java
@@ -101,31 +101,4 @@ public class Product implements Serializable {
         this.cost = cost;
     }
 
-    /**
-     * Check to see if we have enough of this item for an order
-     *
-     * @param amount Number of items being ordered
-     * @return true if enough stock
-     */
-    public boolean canOrder(int amount) {
-        return (itemCount - amount >= 0);
-    }
-
-    /**
-     * Place an order, decrement the available itemCount
-     *
-     * @param amount being ordered
-     * @return if order was successfully processed
-     */
-    public boolean order(int amount) {
-        if (canOrder(amount)) {
-            itemCount = itemCount - amount;
-            setUpdated(true);  // Need to store the updated product information
-
-            // TODO:  add stock management functionality
-            return true;
-        }
-
-        return false;
-    }
 }
