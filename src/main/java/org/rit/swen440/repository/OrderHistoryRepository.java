7091a98c7cfad96fc124195d25398d7dea88863d
Ryan Connors <connorsryanj@gmail.com>
2018-12-07 16:13:41 -0500

    Added new Repositories

---
+++ src/main/java/org/rit/swen440/repository/OrderHistoryRepository.java
@@ -0,0 +1,134 @@
+package org.rit.swen440.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.rit.swen440.dataLayer.OrderHistory;
+
+public class OrderHistoryRepository {
+
+    public static void createRecord(OrderHistory orderHistory) {
+        // Getting Session Object From SessionFactory
+        Session sessionObj = DBSession.getSession();
+        int count = 0;
+        try {
+            // Getting Transaction Object From Session Object
+            sessionObj.beginTransaction();
+
+            sessionObj.save(orderHistory);
+
+            // Committing The Transactions To The Database
+            sessionObj.getTransaction().commit();
+        } catch(Exception sqlException) {
+            if(null != sessionObj.getTransaction()) {
+                sessionObj.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(sessionObj != null) {
+                sessionObj.close();
+            }
+        }
+    }
+
+    // Method 2: This Method Is Used To Display The Records From The Database Table
+    public static List getAllRecords() {
+        List orderHistoryList = new ArrayList();
+        // Getting Session Object From SessionFactory
+        Session sessionObj = DBSession.getSession();
+        try {
+            // Getting Transaction Object From Session Object
+            sessionObj.beginTransaction();
+
+            orderHistoryList = sessionObj.createQuery("FROM OrderHistory").list();
+        } catch(Exception sqlException) {
+            if(null != sessionObj.getTransaction()) {
+                sessionObj.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(sessionObj != null) {
+                sessionObj.close();
+            }
+        }
+        return orderHistoryList;
+    }
+
+    // Method 3: This Method Is Used To Update A Record In The Database Table
+    public static void updateRecord(OrderHistory orderHistory) {
+        // Getting Session Object From SessionFactory
+        Session sessionObj = DBSession.getSession();
+        try {
+            // Getting Transaction Object From Session Object
+            sessionObj.beginTransaction();
+
+            // Creating Transaction Entity
+            OrderHistory orderHistoryObj = (OrderHistory) sessionObj.get(OrderHistory.class, orderHistory.getId());
+            orderHistoryObj = orderHistory;
+
+            // Committing The Transactions To The Database
+            sessionObj.getTransaction().commit();
+        } catch(Exception sqlException) {
+            if(null != sessionObj.getTransaction()) {
+                sessionObj.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(sessionObj != null) {
+                sessionObj.close();
+            }
+        }
+    }
+
+    // Method 4(a): This Method Is Used To Delete A Particular Record From The Database Table
+    public static void deleteRecord(OrderHistory orderHistory) {
+        // Getting Session Object From SessionFactory
+        Session sessionObj = DBSession.getSession();
+        try {
+            // Getting Transaction Object From Session Object
+            sessionObj.beginTransaction();
+
+            OrderHistory orderHistoryObj = (OrderHistory) sessionObj.get(OrderHistory.class, orderHistory.getId());
+            sessionObj.delete(orderHistoryObj);
+
+            // Committing The Transactions To The Database
+            sessionObj.getTransaction().commit();
+        } catch(Exception sqlException) {
+            if(null != sessionObj.getTransaction()) {
+                sessionObj.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(sessionObj != null) {
+                sessionObj.close();
+            }
+        }
+    }
+
+    // Method 5: This Method Is Used To Delete All Records From The Database Table
+    public static void deleteAllRecords() {
+        // Getting Session Object From SessionFactory
+        Session sessionObj = DBSession.getSession();
+        try {
+            // Getting Transaction Object From Session Object
+            sessionObj.beginTransaction();
+
+            Query queryObj = sessionObj.createQuery("DELETE FROM OrderHistory");
+            queryObj.executeUpdate();
+
+            // Committing The Transactions To The Database
+            sessionObj.getTransaction().commit();
+        } catch(Exception sqlException) {
+            if(null != sessionObj.getTransaction()) {
+                sessionObj.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(sessionObj != null) {
+                sessionObj.close();
+            }
+        }
+    }
+}
---
+++ src/main/java/org/rit/swen440/repository/WishListRepository.java
@@ -0,0 +1,134 @@
+package org.rit.swen440.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.rit.swen440.dataLayer.WishList;
+
+public class WishListRepository {
+
+    public static void createRecord(WishList wishList) {
+        // Getting Session Object From SessionFactory
+        Session sessionObj = DBSession.getSession();
+        int count = 0;
+        try {
+            // Getting Transaction Object From Session Object
+            sessionObj.beginTransaction();
+
+            sessionObj.save(wishList);
+
+            // Committing The Transactions To The Database
+            sessionObj.getTransaction().commit();
+        } catch(Exception sqlException) {
+            if(null != sessionObj.getTransaction()) {
+                sessionObj.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(sessionObj != null) {
+                sessionObj.close();
+            }
+        }
+    }
+
+    // Method 2: This Method Is Used To Display The Records From The Database Table
+    public static List getAllRecords() {
+        List wishListList = new ArrayList();
+        // Getting Session Object From SessionFactory
+        Session sessionObj = DBSession.getSession();
+        try {
+            // Getting Transaction Object From Session Object
+            sessionObj.beginTransaction();
+
+            wishListList = sessionObj.createQuery("FROM WishList").list();
+        } catch(Exception sqlException) {
+            if(null != sessionObj.getTransaction()) {
+                sessionObj.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(sessionObj != null) {
+                sessionObj.close();
+            }
+        }
+        return wishListList;
+    }
+
+    // Method 3: This Method Is Used To Update A Record In The Database Table
+    public static void updateRecord(WishList wishList) {
+        // Getting Session Object From SessionFactory
+        Session sessionObj = DBSession.getSession();
+        try {
+            // Getting Transaction Object From Session Object
+            sessionObj.beginTransaction();
+
+            // Creating Transaction Entity
+            WishList wishListObj = (WishList) sessionObj.get(WishList.class, wishList.getId());
+            wishListObj = wishList;
+
+            // Committing The Transactions To The Database
+            sessionObj.getTransaction().commit();
+        } catch(Exception sqlException) {
+            if(null != sessionObj.getTransaction()) {
+                sessionObj.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(sessionObj != null) {
+                sessionObj.close();
+            }
+        }
+    }
+
+    // Method 4(a): This Method Is Used To Delete A Particular Record From The Database Table
+    public static void deleteRecord(WishList wishList) {
+        // Getting Session Object From SessionFactory
+        Session sessionObj = DBSession.getSession();
+        try {
+            // Getting Transaction Object From Session Object
+            sessionObj.beginTransaction();
+
+            WishList wishListObj = (WishList) sessionObj.get(WishList.class, wishList.getId());
+            sessionObj.delete(wishListObj);
+
+            // Committing The Transactions To The Database
+            sessionObj.getTransaction().commit();
+        } catch(Exception sqlException) {
+            if(null != sessionObj.getTransaction()) {
+                sessionObj.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(sessionObj != null) {
+                sessionObj.close();
+            }
+        }
+    }
+
+    // Method 5: This Method Is Used To Delete All Records From The Database Table
+    public static void deleteAllRecords() {
+        // Getting Session Object From SessionFactory
+        Session sessionObj = DBSession.getSession();
+        try {
+            // Getting Transaction Object From Session Object
+            sessionObj.beginTransaction();
+
+            Query queryObj = sessionObj.createQuery("DELETE FROM WishList");
+            queryObj.executeUpdate();
+
+            // Committing The Transactions To The Database
+            sessionObj.getTransaction().commit();
+        } catch(Exception sqlException) {
+            if(null != sessionObj.getTransaction()) {
+                sessionObj.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(sessionObj != null) {
+                sessionObj.close();
+            }
+        }
+    }
+}
