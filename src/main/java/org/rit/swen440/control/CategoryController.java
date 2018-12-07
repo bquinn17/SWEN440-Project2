7bf4a6fcd5f4652d7507ff99da5d8e7d41b7bfc0
Bryan Quinn <blq9064@rit.edu>
2018-12-07 17:44:00 -0500

    Implement functionality of category controller and initailize categories and products

--- src/main/java/org/rit/swen440/control/CategoryController.java
+++ src/main/java/org/rit/swen440/control/CategoryController.java
@@ -3,36 +3,35 @@ package org.rit.swen440.control;
 import org.rit.swen440.dataLayer.Category;
 import org.rit.swen440.repository.CategoryRepository;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class CategoryController {
 
+    public CategoryController() {
+        if (CategoryRepository.getAllRecords().size() == 0) {
+            initializeCategories();
+        }
+    }
 
+    private void initializeCategories() {
+        Category eight_track_cat = new Category();
+        eight_track_cat.setName("Eight Track Tapes");
+        eight_track_cat.setDescription("Our collection of the best 8-track tapes of era.  Listen to the BeeJees, Peter Frampton and other classics.");
+        CategoryRepository.createRecord(eight_track_cat);
+
+        Category toys_cat = new Category();
+        toys_cat.setName("Toys");
+        toys_cat.setDescription("Our collection of the best 8-track tapes of era.  Listen to the BeeJees, Peter Frampton and other classics.");
+        CategoryRepository.createRecord(toys_cat);
+    }
 
     /**
      * Get a list of all category names
      *
      * @return list of categories
      */
-    public static List<Category> getCategories() {
-
+    public List<Category> getCategories() {
         return CategoryRepository.getAllRecords();
-    }
-
-    /**
-     * Get the description of the named category
-     *
-     * @param category name
-     * @return description
-     */
-    public static String getCategoryDescription(String category) {
-        Optional<Category> match = categories.stream().filter(c -> c.getName().equalsIgnoreCase(category)).findFirst();
-        return match.map(Category::getDescription).orElse(null);
     }
 
     /**
@@ -41,53 +40,25 @@ public class CategoryController {
      * @param name
      * @return Category, if present
      */
-    public static Optional<Category> findCategory(String name) {
-        return categories.stream()
-                .filter(c -> c.getName().equalsIgnoreCase(name))
-                .findFirst();
+    public Category findCategory(String name) {
+        Category matchingCategory = null;
+        List<Category> categories = CategoryRepository.getAllRecords();
+        for (Category category : categories) {
+            if (category.getName().equalsIgnoreCase(name)) {
+                matchingCategory = category;
+            }
+        }
+        return matchingCategory;
     }
 
     /**
      * Loop through all our categories and write any product records that
      * have been updated.
      */
-    public static void writeCategories() {
+    public void updateCategories(List<Category> categories) {
         for (Category category : categories) {
-            writeProducts(category.getProducts());
+            CategoryRepository.updateRecord(category);
         }
-    }
-
-    /**
-     * Get the category object for this directory
-     *
-     * @param path directory
-     * @return Category object, if .cat file exists
-     */
-    private static Optional<Category> getCategory(Path path) {
-        DirectoryStream.Filter<Path> catFilter = new DirectoryStream.Filter<Path>() {
-            @Override
-            public boolean accept(Path path) throws IOException {
-                return path.toString().toLowerCase().endsWith("cat");
-            }
-        };
-
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path, catFilter)) {
-            for (Path file : stream) {
-                // read the file
-                BufferedReader reader = Files.newBufferedReader(file, Charset.forName("US-ASCII"));
-                Category category = new Category();
-
-                category.setName(reader.readLine());
-                category.setDescription(reader.readLine());
-                category.setProducts(loadProducts(path));
-
-                return Optional.of(category);
-            }
-        } catch (IOException | DirectoryIteratorException e) {
-            System.err.println(e);
-        }
-
-        return Optional.empty();
     }
 
 }
--- src/main/java/org/rit/swen440/control/ProductController.java
+++ src/main/java/org/rit/swen440/control/ProductController.java
@@ -3,6 +3,7 @@ package org.rit.swen440.control;
 import org.rit.swen440.dataLayer.Category;
 import org.rit.swen440.dataLayer.Product;
 import org.rit.swen440.repository.ProductRepository;
+import org.rit.swen440.repository.ProductRepository;
 import org.rit.swen440.repository.CategoryRepository;
 
 import java.io.BufferedReader;
@@ -19,6 +20,46 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class ProductController {
+
+    public ProductController(){
+        if (ProductRepository.getAllRecords().size() == 0) {
+            initializeProducts();
+        }
+    }
+
+    private void initializeProducts() {
+        CategoryController categoryController = new CategoryController();
+
+        Product darkSide = new Product();
+        darkSide.setItemCount(13);
+        darkSide.setReorderAmount(10);
+        darkSide.setReorderThreshold(2);
+        darkSide.setTitle("Dark Side of the Moon");
+        darkSide.setDescription("A classic Pink Floyd album");
+        darkSide.setCost(new BigDecimal(8.99));
+        darkSide.setCategory(categoryController.findCategory("Eight Track Tapes"));
+        ProductRepository.createRecord(darkSide);
+
+        Product theCars = new Product();
+        theCars.setItemCount(13);
+        theCars.setReorderAmount(10);
+        theCars.setReorderThreshold(4);
+        theCars.setTitle("The Cars");
+        theCars.setDescription("The Cars is the debut album by the American new wave band the Cars. It was released on June 6, 1978 on Elektra Records. The album, which featured the three charting singles \"Just What I Needed,\" \"My Best Friend's Girl,\" and \"Good Times Roll,\" as well as several album-oriented rock radio hits, was a major success for the band, remaining on the charts for 139 weeks. It has been recognized as one of the band's best albums. -- wikipedia");
+        theCars.setCost(new BigDecimal(4.95));
+        theCars.setCategory(categoryController.findCategory("Eight Track Tapes"));
+        ProductRepository.createRecord(theCars);
+
+        Product rubicsCube = new Product();
+        rubicsCube.setItemCount(50);
+        rubicsCube.setReorderAmount(10);
+        rubicsCube.setReorderThreshold(45);
+        rubicsCube.setTitle("Rubic's Cube");
+        rubicsCube.setDescription("A mindbending toy, turn the sides to move color tiles.  The objective is to arange the cube so that each side is one color.");
+        rubicsCube.setCost(new BigDecimal(5.00));
+        rubicsCube.setCategory(categoryController.findCategory("Toys"));
+        ProductRepository.createRecord(rubicsCube);
+    }
 
     /**
      * Return a list of Products based on the provided category.
