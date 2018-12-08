c928208788812399c9d3c2ca13e8ca045640a739
Bryan Quinn <blq9064@rit.edu>
2018-12-08 13:16:57 -0500

    Add logging configurations

--- src/main/java/org/rit/swen440/OrderSystem.java
+++ src/main/java/org/rit/swen440/OrderSystem.java
@@ -7,21 +7,15 @@ import java.io.IOException;
 
 public class OrderSystem {
     public static void main(String[] args) {
-        try {
-            System.getProperties().load(new FileInputStream("orderSys.properties"));
-            System.out.println("Hello");
-            MenuManager mgr = new MenuManager();
-            int currentLevel = 0;
-            boolean done = false;
-            do {
-                done = mgr.loadLevel(currentLevel);
-            } while (!done);
 
-            System.out.println("Thank you for shopping at Hippolyta.com!");
+        MenuManager mgr = new MenuManager();
+        int currentLevel = 0;
+        boolean done = false;
+        do {
+            done = mgr.loadLevel(currentLevel);
+        } while (!done);
 
-        } catch (IOException e) {
-            System.err.println("orderSys.properties not found.");
-        }
+        System.out.println("Thank you for shopping at Hippolyta.com!");
 
     }
 }
